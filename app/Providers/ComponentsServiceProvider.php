439c33838f748dd88805d6a0eccebfd45a8429b8
Victor Macedo <victor.macedo@betalabs.com.br>
2020-05-15 19:22:01 -0300

    Including components service provider

---
+++ app/Providers/ComponentsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComponentsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'engine-components');
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/courier'),
+        ]);
+    }
+}
